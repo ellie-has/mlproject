65d0eb8a2fc7b0b255b016802f92e29c829a97a5
ellie-has <38140393+ellie-has@users.noreply.github.com>
2025-01-02 13:37:42 +0000

    utils

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,4 @@
-import os
 import sys
-import numpy as np 
-import pandas as pd
 import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
@@ -111,3 +108,46 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params,
     
     except Exception as e:
         raise CustomException(e, sys)
+
+
+# def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+#     """
+#     Simple model evaluation function that trains each model with specified parameters
+#     and returns their performance scores.
+    
+#     Args:
+#         X_train, y_train: Training data
+#         X_test, y_test: Test data
+#         models: Dictionary of models to evaluate
+#         params: Dictionary of parameters for each model
+    
+#     Returns:
+#         Dictionary with model names as keys and R2 scores as values
+#     """
+#     try:
+#         report = {}
+        
+#         for model_name, model in models.items():
+#             # Set the parameters directly for the model
+#             if params[model_name]:
+#                 # Take the first value for each parameter if multiple are provided
+#                 best_params = {
+#                     param: values[0] if isinstance(values, list) else values
+#                     for param, values in params[model_name].items()
+#                 }
+#                 model.set_params(**best_params)
+            
+#             # Train the model
+#             model.fit(X_train, y_train)
+            
+#             # Make predictions and calculate score
+#             y_test_pred = model.predict(X_test)
+#             test_model_score = r2_score(y_test, y_test_pred)
+            
+#             # Store the score
+#             report[model_name] = test_model_score
+            
+#         return report
+        
+#     except Exception as e:
+#         raise CustomException(e, sys)
