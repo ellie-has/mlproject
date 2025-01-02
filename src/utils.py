862193b77c58f6997195fcf5dbc54c8ad44fdec8
ellie-has <38140393+ellie-has@users.noreply.github.com>
2025-01-02 12:55:29 +0000

    utils

--- src/utils.py
+++ src/utils.py
@@ -3,9 +3,8 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
-import pickle
 from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from src.exception import CustomException
 
 
@@ -22,26 +21,93 @@ def save_object(obj, file_path):
     except Exception as e:
         raise CustomException(f"Save object failed: {str(e)}", sys)
     
-    def evaluate_models(X_train, y_train, X_test, y_test, models, params):
-        """
-        Evaluate a list of models on the given data
-        Args:
-            X_train: Training features
-            y_train: Training target
-            X_test: Test features
-            y_test: Test target
-            models: List of models to evaluate
-            param: Dictionary of hyperparameters for the models
-        """
-        try:
-            model_report = {}
-            for model in range(len(list(models))):
-                model = list(models.values())[model]
-                param = list(params.keys())[model]
-                grid = GridSearchCV(model, param, refit=True, verbose=3)
-                grid.fit(X_train, y_train)
-                model_report[model] = grid.score(X_test, y_test)
-            return model_report
+
+
+
+# def evaluate_models(X_train,y_train,X_test,y_test,models,params,cv=3,n_jobs=3,verbose=1,refit=False):
+#     try:
+#         report={}
+#         for i in range(len(models)):
+            
+#             model=list(models.values())[i]
+#             para=params[list(models.keys())[i]]
+            
+#             gs = GridSearchCV(model,para,cv=cv,n_jobs=n_jobs,verbose=verbose,refit=refit)
+#             gs.fit(X_train,y_train)
+            
+#             model.set_params(**gs.best_params_)
+#             model.fit(X_train,y_train)
+#             y_test_pred=model.predict(X_test)
+
+#             test_model_score=r2_score(y_test, y_test_pred)
+#             report[list(models.keys())[i]]=test_model_score
+     
+
+#         return report
+#     except Exception as e:
+#         raise CustomException(e,sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params, 
+                   n_iter=10, cv=3, n_jobs=3, verbose=1):
+    """
+    Evaluate multiple models using RandomizedSearchCV for parameter tuning
+    
+    Args:
+        X_train, y_train: Training data
+        X_test, y_test: Test data
+        models: Dictionary of models to evaluate
+        params: Dictionary of parameters for each model
+        n_iter: Number of parameter settings sampled
+        cv: Number of cross-validation folds
+        n_jobs: Number of parallel jobs
+        verbose: Verbosity level
+    
+    Returns:
+        Dictionary with model names as keys and best scores as values
+    """
+    try:
+        report = {}
         
-        except Exception as e:
-            raise CustomException(e, sys)
+        for i in range(len(models)):
+            model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+            param_dist = params[model_name]
+            
+            # Skip parameter search if no parameters to tune
+            if not param_dist:
+                model.fit(X_train, y_train)
+                y_test_pred = model.predict(X_test)
+                test_model_score = r2_score(y_test, y_test_pred)
+                report[model_name] = test_model_score
+                continue
+                
+            # Use RandomizedSearchCV for parameter tuning
+            random_search = RandomizedSearchCV(
+                estimator=model,
+                param_distributions=param_dist,
+                n_iter=n_iter,
+                cv=cv,
+                n_jobs=n_jobs,
+                verbose=verbose,
+                random_state=42
+            )
+            
+            # Fit and evaluate model
+            random_search.fit(X_train, y_train)
+            best_model = random_search.best_estimator_
+            y_test_pred = best_model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+            
+            # Store results
+            report[model_name] = test_model_score
+            
+            # Optional: Print best parameters for each model
+            if verbose:
+                print(f"\nBest parameters for {model_name}:")
+                print(random_search.best_params_)
+                print(f"Best score: {test_model_score}")
+        
+        return report
+    
+    except Exception as e:
+        raise CustomException(e, sys)
